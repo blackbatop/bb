d563c72f7224508080b0471e1c2224b2b44d705e
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-05-11 00:35:23 -0500

    Update interface.py
    Update interface.py
    
    Update interface.py
    
    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -136,6 +136,10 @@ class CarInterface(CarInterfaceBase):
         ret.openpilotLongitudinalControl = True
         ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_CAM_LONG
 
+      # CAM_LONG flag only if camera-capable and conditional_experimental_mode enabled
+      if candidate in CAMERA_ACC_CAR and getattr(frogpilot_toggles, "conditional_experimental_mode", False):
+        ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_CAM_LONG
+
     elif candidate in SDGM_CAR:
       ret.longitudinalTuning.kiV = [0., 0., 0.]  # TODO: tuning
       ret.experimentalLongitudinalAvailable = False
@@ -147,7 +151,12 @@ class CarInterface(CarInterfaceBase):
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_SDGM
 
     else:  # ASCM, OBD-II harness
-      ret.openpilotLongitudinalControl = not frogpilot_toggles.disable_openpilot_long
+      try:
+        disable_long = frogpilot_toggles.disable_openpilot_long
+      except AttributeError:
+        disable_long = params.get_bool("DisableOpenpilotLongitudinal")
+
+      ret.openpilotLongitudinalControl = True
       ret.networkLocation = NetworkLocation.gateway
       ret.radarUnavailable = RADAR_HEADER_MSG not in fingerprint[CanBus.OBSTACLE] and not docs
       ret.pcmCruise = False  # stock non-adaptive cruise control is kept off
@@ -269,7 +278,11 @@ class CarInterface(CarInterfaceBase):
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_CAM
       ret.minEnableSpeed = -1
       ret.pcmCruise = False
-      ret.openpilotLongitudinalControl = not frogpilot_toggles.disable_openpilot_long
+      try:
+        disable_long = frogpilot_toggles.disable_openpilot_long
+      except AttributeError:
+        disable_long = params.get_bool("DisableOpenpilotLongitudinal")
+      ret.openpilotLongitudinalControl = True
       ret.stoppingControl = True
       ret.autoResumeSng = True
 
@@ -289,12 +302,18 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate in CC_ONLY_CAR:
       ret.flags |= GMFlags.CC_LONG.value
-      ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_CC_LONG
       ret.radarUnavailable = True
       ret.experimentalLongitudinalAvailable = False
       ret.minEnableSpeed = 24 * CV.MPH_TO_MS
-      ret.openpilotLongitudinalControl = not frogpilot_toggles.disable_openpilot_long
+      try:
+        disable_long = frogpilot_toggles.disable_openpilot_long
+      except AttributeError:
+        disable_long = params.get_bool("DisableOpenpilotLongitudinal")
+      ret.openpilotLongitudinalControl = True
       ret.pcmCruise = False
+
+      if not ret.enableGasInterceptor:
+        ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_CC_LONG
 
       if not ret.enableGasInterceptor and candidate in CC_ONLY_CAR: #redneck tuning
         ret.longitudinalTuning.kpBP = [10.7, 10.8, 28.]  # 10.7 m/s == 24 mph
