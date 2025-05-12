a8767be7b12477d8011760a4333e7b9b5b5ea8c8
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-05-12 13:15:49 -0500

    Revert "panda fix"
    This reverts commit 2d23f740cc731fd290002660aa50113f7c711657.

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -286,6 +286,7 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate in CC_ONLY_CAR:
       ret.flags |= GMFlags.CC_LONG.value
+      ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_CC_LONG
       ret.radarUnavailable = True
       ret.experimentalLongitudinalAvailable = False
       ret.minEnableSpeed = 24 * CV.MPH_TO_MS
