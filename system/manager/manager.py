1c257d7065bcbd41f4026a8fe87d9e9285db6067
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-05-14 20:26:15 -0500

    Revert "Update manager.py"
    This reverts commit ab16a970e024dcc38165223034b4d2410e14e3a2.

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -177,11 +177,10 @@ def manager_thread() -> None:
       params_memory.clear_all(ParamKeyType.CLEAR_ON_OFFROAD_TRANSITION)
 
     # update onroad params, which drives pandad's safety setter thread
-    if started and not started_prev:
-      cloudlog.warning("Delaying onroad process start for GM")
-      time.sleep(2)
-      write_onroad_params(started, params)
-    elif not started and started_prev:
+    if started != started_prev:
+      if started:
+        cloudlog.warning("Delaying onroad process start for GM")
+        time.sleep(2)
       write_onroad_params(started, params)
 
     started_prev = started
