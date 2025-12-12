a1f7b451a686dda48a3b61f09dc40d9db08aab5d
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-12-12 01:01:16 -0600

    Trailer Load Gas Tuning

--- frogpilot/controls/lib/frogpilot_acceleration.py
+++ frogpilot/controls/lib/frogpilot_acceleration.py
@@ -8,36 +8,36 @@ def cubic_interp(x, xp, fp):
          return fp[0]
      elif x >= xp[-1]:
          return fp[-1]
- 
+
      # Find interval
      i = np.searchsorted(xp, x) - 1
      i = max(0, min(i, len(xp)-2))  # clamp the index
- 
+
      # Normalized position
      t = (x - xp[i]) / float(xp[i+1] - xp[i])
- 
+
      # Hermite cubic formula
      return fp[i]*(1 - 3*t**2 + 2*t**3) + fp[i+1]*(3*t**2 - 2*t**3)
- 
+
 def akima_interp(x, xp, fp):
      """Akima-inspired interpolation with reduced overshoot characteristics."""
      if x <= xp[0]:
          return fp[0]
      elif x >= xp[-1]:
          return fp[-1]
- 
+
      i = np.searchsorted(xp, x) - 1
      i = max(0, min(i, len(xp)-2))  # clamp the index
- 
+
      t = (x - xp[i]) / float(xp[i+1] - xp[i])
- 
+
      # Quintic polynomial to reduce overshoot
      t2 = t*t
      t4 = t2*t2
      t3 = t2*t
      return (fp[i]*(1 - 10*t3 + 15*t4 - 6*t3*t2)
              + fp[i+1]*(10*t3 - 15*t4 + 6*t3*t2))
- 
+
 from openpilot.selfdrive.controls.lib.longitudinal_planner import A_CRUISE_MIN, get_max_accel
 
 from openpilot.frogpilot.common.frogpilot_variables import CITY_SPEED_LIMIT
@@ -47,14 +47,18 @@ A_CRUISE_MIN_SPORT = A_CRUISE_MIN * 2
 
                        # MPH = [0.0,  11,  22,  34,  45,  56,  89]
 A_CRUISE_MAX_BP_CUSTOM =       [0.0,  5., 10., 15., 20., 25., 40.]
-A_CRUISE_MAX_VALS_ECO =        [2.0, 1.5, 1.0, 0.8, 0.6, 0.4, 0.2]
-A_CRUISE_MAX_VALS_SPORT =      [3.0, 2.5, 2.0, 1.5, 1.0, 0.8, 0.6]
+A_CRUISE_MAX_VALS_ECO_EV =     [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+A_CRUISE_MAX_VALS_SPORT_EV =   [1.25, 1.25, 1.25, 1.25, 1.5, 1.5, 2.0]
+A_CRUISE_MAX_VALS_ECO_GAS =    [2.0, 1.5, 1.0, 0.8, 0.6, 0.4, 0.2]
+A_CRUISE_MAX_VALS_SPORT_GAS =  [3.0, 2.5, 2.0, 1.5, 1.0, 0.8, 0.6]
 
-def get_max_accel_eco(v_ego):
-  return float(akima_interp(v_ego, A_CRUISE_MAX_BP_CUSTOM, A_CRUISE_MAX_VALS_ECO))
+def get_max_accel_eco(v_ego, ev_tuning=True):
+  cruise_vals = A_CRUISE_MAX_VALS_ECO_EV if ev_tuning else A_CRUISE_MAX_VALS_ECO_GAS
+  return float(akima_interp(v_ego, A_CRUISE_MAX_BP_CUSTOM, cruise_vals))
 
-def get_max_accel_sport(v_ego):
-  return float(akima_interp(v_ego, A_CRUISE_MAX_BP_CUSTOM, A_CRUISE_MAX_VALS_SPORT))
+def get_max_accel_sport(v_ego, ev_tuning=True):
+  cruise_vals = A_CRUISE_MAX_VALS_SPORT_EV if ev_tuning else A_CRUISE_MAX_VALS_SPORT_GAS
+  return float(akima_interp(v_ego, A_CRUISE_MAX_BP_CUSTOM, cruise_vals))
 
 def get_max_accel_low_speeds(max_accel, v_cruise):
   return float(akima_interp(v_cruise, [0., CITY_SPEED_LIMIT / 2, CITY_SPEED_LIMIT], [max_accel / 4, max_accel / 2, max_accel]))
@@ -75,22 +79,23 @@ class FrogPilotAcceleration:
   def update(self, v_ego, sm, frogpilot_toggles):
     eco_gear = sm["frogpilotCarState"].ecoGear
     sport_gear = sm["frogpilotCarState"].sportGear
+    ev_tuning = frogpilot_toggles.ev_tuning
 
     if sm["frogpilotCarState"].trafficModeEnabled:
       self.max_accel = get_max_accel(v_ego)
     elif frogpilot_toggles.map_acceleration and (eco_gear or sport_gear):
       if eco_gear:
-        self.max_accel = get_max_accel_eco(v_ego)
+        self.max_accel = get_max_accel_eco(v_ego, ev_tuning)
       else:
         if frogpilot_toggles.acceleration_profile == 2:
-          self.max_accel = get_max_accel_sport(v_ego)
+          self.max_accel = get_max_accel_sport(v_ego, ev_tuning)
         else:
           self.max_accel = get_max_allowed_accel(v_ego)
     else:
       if frogpilot_toggles.acceleration_profile == 1:
-        self.max_accel = get_max_accel_eco(v_ego)
+        self.max_accel = get_max_accel_eco(v_ego, ev_tuning)
       elif frogpilot_toggles.acceleration_profile == 2:
-        self.max_accel = get_max_accel_sport(v_ego)
+        self.max_accel = get_max_accel_sport(v_ego, ev_tuning)
       elif frogpilot_toggles.acceleration_profile == 3:
         self.max_accel = get_max_allowed_accel(v_ego)
       else:
