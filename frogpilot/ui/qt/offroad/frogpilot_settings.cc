89f6f7a42b80017d11b8b2bcc2d7a79f9407edf8
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2026-02-07 22:59:56 -0600

    User adjustable offsets

--- frogpilot/ui/qt/offroad/frogpilot_settings.cc
+++ frogpilot/ui/qt/offroad/frogpilot_settings.cc
@@ -286,6 +286,7 @@ void FrogPilotSettingsWindow::updateVariables() {
     longitudinalActuatorDelay = CP.getLongitudinalActuatorDelay();
     startAccel = CP.getStartAccel();
     steerActuatorDelay = CP.getSteerActuatorDelay();
+    steerOffset = 0.0f;
     steerKp = CP.getLateralTuning().which() == cereal::CarParams::LateralTuning::PID ? CP.getLateralTuning().getPid().getKpV()[0] : 0.6;
     steerRatio = CP.getSteerRatio();
     stopAccel = CP.getStopAccel();
@@ -297,6 +298,7 @@ void FrogPilotSettingsWindow::updateVariables() {
 
     float currentDelayStock = params.getFloat("SteerDelayStock");
     float currentFrictionStock = params.getFloat("SteerFrictionStock");
+    float currentSteerOffsetStock = params.getFloat("SteerOffsetStock");
     float currentKPStock = params.getFloat("SteerKPStock");
     float currentLatAccelStock = params.getFloat("SteerLatAccelStock");
     float currentLongDelayStock = params.getFloat("LongitudinalActuatorDelayStock");
@@ -319,6 +321,13 @@ void FrogPilotSettingsWindow::updateVariables() {
         params.putFloat("SteerFriction", friction);
       }
       params.putFloat("SteerFrictionStock", friction);
+    }
+
+    if (currentSteerOffsetStock != steerOffset) {
+      if (params.getFloat("SteerOffset") == currentSteerOffsetStock) {
+        params.putFloat("SteerOffset", steerOffset);
+      }
+      params.putFloat("SteerOffsetStock", steerOffset);
     }
 
     if (currentKPStock != steerKp && steerKp != 0) {
